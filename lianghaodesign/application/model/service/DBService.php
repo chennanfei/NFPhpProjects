b3de7737877acbe52dd54eed8416a3800dda98be
Chen Nanfei <8210041@sina.com>
2015-08-31 23:11:33 +0800

    added db port

--- lianghaodesign/application/model/service/DBService.php
+++ lianghaodesign/application/model/service/DBService.php
@@ -3,10 +3,15 @@ require_once 'Scorpion/DB/NFDBService.php';
 
 class DBService extends NFDBService {
     protected function getConfig() {
+        $port = getenv('DATABASE_PORT');
+        if (!$port) {
+            $port = 3306;
+        }
         return array(
             'dbname'    => getenv('DATABASE_NAME'),
             'driver'    => 'pdo_mysql',
             'host'      => getenv('DATABASE_HOST'),
+            'port'      => $port,
             'password'  => getenv('DATABASE_PASSWORD'),
             'user'      => getenv('DATABASE_USER')
         );
